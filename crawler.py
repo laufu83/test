04bedd17e2ac793d996315335f20ccbc32203b7e
laufu83 <18044722@qq.com>
2026-05-05 17:37:13 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -13,7 +13,7 @@ DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "postgres")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
-TABLE_NAME = "movies.vod_data"
+TABLE_NAME = "movies.vod_dytt"
 MAX_THREAD = 5
 # ==========================================================
 API_BASE_URL = "https://api.xinlangapi.com/xinlangapi.php/provide/vod/from/xlm3u8/"
